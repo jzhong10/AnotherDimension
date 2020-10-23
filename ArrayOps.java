fd690f94a24173f747f62e1b2253290d5e093248
jzhong10 <junzhong2003@gmail.com>
2020-10-23 13:18:03 -0400

    Tested and implemented largest() function

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,12 +1,24 @@
 public class ArrayOps {
+
     public static void main(String[] args) {
 
     }
+
     public static int sum(int[] arr) {
       int sum = 0;
       for (int i = 0; i<arr.length; i++) {
         sum += arr[i];
       }
       return sum;
+    }
+
+    public static int largest(int[] arr) {
+      int large = arr[0];
+      for (int i = 1; i<arr.length; i++) {
+        if (arr[i]>large) {
+          large = arr[i];
+        }
+      }
+      return large;
     }
 }
--- Tester.java
+++ Tester.java
@@ -2,5 +2,6 @@ public class Tester {
     public static void main(String[] args) {
         int[]a1 = {1, 2, 3, 4};
         System.out.println(ArrayOps.sum(a1));
+        System.out.println(ArrayOps.largest(a1));
     }
 }
